8cbf95fa4aea69b7c60c549ab988c3db5a84de32
adnankg <129622411+adnankg@users.noreply.github.com>
2023-04-21 11:07:03 +0600

    Add files via upload

---
+++ Pythone2 eid-mubarak.py
@@ -0,0 +1,16 @@
+Open in CMD 
+^^
+
+
+db_user = "Eid Mubarak"
+db_passwd = "Happy Eid"
+
+
+username = input ('Say,  Eid Mubarak: ')
+password = input ('Say,  Happy Eid: ')
+
+
+if username == db_user and password == db_passwd:
+    print ('Happy Eid')
+else:
+    print ('Happy Eid Too ^_^ ')
